dcec04459f4e6833be07056ec1fb842dba771e7e
zlfn <ung@zlfn.space>
2024-09-16 00:49:00 +0900

    Update xtask-build-rom & Write a rand-text example

--- xtask/build-rom/src/main.rs
+++ xtask/build-rom/src/main.rs
@@ -1,8 +1,9 @@
 use core::str;
-use std::{fs::{self, File, OpenOptions}, io::{ErrorKind, Write}, process::{self, Command}, str::FromStr};
+use std::{fs::{self, File}, io::{ErrorKind, Write}, process::{self, Command}, str::FromStr};
 use clap::Parser;
 
 use colored::Colorize;
+use project_root::get_project_root;
 use tree_sitter::{self, Query, QueryCursor};
 
 ///Build GB ROM from Rust
@@ -41,12 +42,15 @@ fn main() {
     let args = Args::parse();
     let build_from = BuildChain::from_str(&args.from).unwrap();
 
+    let root = get_project_root().unwrap();
+    let root = root.to_str().unwrap();
+
     fs::create_dir_all("./out").unwrap();
 
     if build_from <= BuildChain::Rust {
         let bundle_result = Command::new("rust_bundler_cp")
             .args([
-                "--input", "./source",
+                "--input", ".",
             ])
             .output()
             .unwrap();
@@ -68,9 +72,9 @@ fn main() {
 
         let libcompiler = Command::new("find")
             .args([
-                "./ext/rust-deps/target/avr-unknown-gnu-atmega328/release/deps",
-                "-name",
-                "libcompiler*.rlib"
+                format!("{}/ext/rust-deps/target/avr-unknown-gnu-atmega328/release/deps", root),
+                "-name".to_string(),
+                "libcompiler*.rlib".to_string()
             ])
             .output()
             .unwrap()
@@ -79,9 +83,9 @@ fn main() {
 
         let libcore = Command::new("find")
             .args([
-                "./ext/rust-deps/target/avr-unknown-gnu-atmega328/release/deps",
-                "-name",
-                "libcore*.rlib"
+                format!("{}/ext/rust-deps/target/avr-unknown-gnu-atmega328/release/deps", root),
+                "-name".to_string(),
+                "libcore*.rlib".to_string()
             ])
             .output()
             .unwrap()
@@ -90,9 +94,9 @@ fn main() {
 
         let liballoc = Command::new("find")
             .args([
-                "./ext/rust-deps/target/avr-unknown-gnu-atmega328/release/deps",
-                "-name",
-                "liballoc*.rlib"
+                format!("{}/ext/rust-deps/target/avr-unknown-gnu-atmega328/release/deps", root),
+                "-name".to_string(),
+                "liballoc*.rlib".to_string()
             ])
             .output()
             .unwrap()
@@ -108,7 +112,7 @@ fn main() {
                 "-C", "panic=abort",
                 "-Z", "unstable-options",
 
-                "-L", "dependency=./ext/rust-deps/target/avr-unknown-gnu-atmega328/release/deps",
+                "-L", format!("dependency={}/ext/rust-deps/target/avr-unknown-gnu-atmega328/release/deps", root).as_str(),
                 "--extern", format!("noprelude:compiler_builtins={}", libcompiler).as_str(),
                 "--extern", format!("noprelude:core={}", libcore).as_str(),
                 "--extern", format!("noprelude:alloc={}", liballoc).as_str(),
@@ -129,7 +133,7 @@ fn main() {
     }
 
     if build_from <= BuildChain::LLVM {
-        let llvm_status = Command::new("./ext/llvm-project/llvm/build/bin/llvm-cbe")
+        let llvm_status = Command::new(format!("{}/ext/llvm-project/llvm/build/bin/llvm-cbe", root))
             .args([
                 "--cbe-declare-locals-late",
                 "./out/out.ll",
@@ -147,19 +151,29 @@ fn main() {
         }
 
         //C postprocessing for SDCC
-        //Command::new("sed 's/static __forceinline/inline/g' -i ./out/main.c").status().unwrap();
-        //Command::new("sed 's/uint8_t\\* memset(uint8_t\\*, uint32_t, uint16_t);/inline uint8_t\\* memset(uint8_t\\* dst, uint8_t c, uint16_t sz) {uint8_t \\*p = dst; while (sz--) *p++ = c; return dst; }/g' -i ./out/main.c").status().unwrap();
-        //Command::new("sed '/__noreturn void rust_begin_unwind(struct l_struct_core_KD__KD_panic_KD__KD_PanicInfo\\* llvm_cbe_info)/{:a;N;/__builtin_unreachable/{N;N;d};/  }/b;ba}' -i ./out/main.c").status().unwrap();
         fs::copy("./out/out.c", "./out/pre.c").unwrap();
         let postprocess_status = treesitter_process();
 
         if postprocess_status.is_err() {
-            println!("{}", "[sed] C postprocess for SDCC failed".red());
+            println!("{}", "[treesitter] C postprocess for SDCC failed".red());
             process::exit(1);
         }
         else {
-            println!("{}", "[sed] C postprocess for SDCC succeeded".green());
+            println!("{}", "[treesitter] C postprocess for SDCC succeeded".green());
         }
+
+        Command::new("sed")
+            .args(["s/static __forceinline/inline/g' -i ./out/out.c"])
+            .status()
+            .unwrap();
+        Command::new("sed")
+            .args(["s/uint8_t\\* memset(uint8_t\\*, uint32_t, uint16_t);/inline uint8_t\\* memset(uint8_t\\* dst, uint8_t c, uint16_t sz) {uint8_t \\*p = dst; while (sz--) *p++ = c; return dst; }/g' -i ./out/out.c"])
+            .status()
+            .unwrap();
+        Command::new("sed")
+            .args(["/__noreturn void rust_begin_unwind(struct l_struct_core_KD__KD_panic_KD__KD_PanicInfo\\* llvm_cbe_info)/{:a;N;/__builtin_unreachable/{N;N;d};/  }/b;ba}' -i ./out/out.c"])
+            .status()
+            .unwrap();
     }
 
     if build_from <= BuildChain::C {
@@ -171,6 +185,8 @@ fn main() {
                 "-D", "__builtin_unreachable()=while(1);",
                 "--out-fmt-ihx",
                 "--max-allocs-per-node", "2000",
+                "--disable-warning", "110",
+                "--disable-warning", "126",
                 "--allow-unsafe-read",
                 "--opt-code-speed",
                 "--no-std-crt0",
@@ -198,8 +214,9 @@ fn main() {
     }
 
     if build_from <= BuildChain::ASM {
-        let lcc_status = Command::new("./ext/bin/lcc")
+        let lcc_status = Command::new(format!("{}/ext/bin/lcc", root))
             .args([
+                "-msm83:gb",
                 "-o", "./out/out.gb",
                 "./out/out.asm"
             ])
@@ -218,22 +235,21 @@ fn main() {
     }
 }
 
-fn treesitter_process() -> Result<(), std::io::Error> {
-    let mut code = fs::read_to_string("./out/out.c")?;
-    let code_bytes = code.clone();
-    let code_bytes = code_bytes.as_bytes();
+fn parse_declarator_attributes(code: &mut String, code_bytes: &[u8]) {
     let mut parser = tree_sitter::Parser::new();
     parser.set_language(&tree_sitter_c::LANGUAGE.into()).unwrap();
 
     let tree = parser.parse(code_bytes, None).unwrap();
-    let query = Query::new(&tree_sitter_c::LANGUAGE.into(), "(function_declarator(identifier)@iden)@decl").unwrap();
+    let query = Query::new(&tree_sitter_c::LANGUAGE.into(), "(function_declarator(identifier)@a)@b").unwrap();
     let mut cursor = QueryCursor::new();
 
     let mut diff: i32 = 0;
 
     for qm in cursor.matches(&query, tree.root_node(), code_bytes) {
-        let node = qm.captures[1].node;
-        let identifier = node.utf8_text(&code_bytes).unwrap();
+        let declarator_node = qm.captures[0].node;
+        let identifier_node = qm.captures[1].node;
+        let declarator = declarator_node.utf8_text(&code_bytes).unwrap();
+        let identifier = identifier_node.utf8_text(&code_bytes).unwrap();
         let attributes: Vec<_> = identifier.split("_AC___").collect(); //` __`
 
         if attributes.len() < 2 {
@@ -242,19 +258,69 @@ fn treesitter_process() -> Result<(), std::io::Error> {
 
         match &attributes[..] {
             [] | [_] => {},
-            [name, attrib @ ..] => {
-                code.replace_range(((node.start_byte() as i32) + diff) as usize..((node.end_byte() as i32) + diff) as usize, name);
-                diff += name.len() as i32 - node.byte_range().len() as i32;
+            [_, attrib @ ..] => {
                 let attrib = format!("__{}", attrib.join(" __"));
                 let attrib = attrib
                     .replace("_AC_", " ")
                     .replace("_IC_", "(")
                     .replace("_JC_", ")")
                     .replace("_MC_", ",");
-                println!("{:?}", attrib);
+
+                let start = declarator.find("_AC_").unwrap();
+                let end = declarator.find("(").unwrap();
+                let mut declarator = String::from(declarator);
+                declarator.replace_range(start..end, "");
+                
+                code.replace_range(
+                    ((declarator_node.start_byte() as i32) + diff) as usize..
+                    ((declarator_node.end_byte() as i32) + diff) as usize, 
+                    &format!("{} {}", declarator, attrib));
+                diff += (declarator.len() + attrib.len() + 1) as i32 - declarator_node.byte_range().len() as i32;
             }
         }
     }
+}
+
+fn parse_call_expression_attributes(code: &mut String, code_bytes: &[u8]) {
+    let mut parser = tree_sitter::Parser::new();
+    parser.set_language(&tree_sitter_c::LANGUAGE.into()).unwrap();
+
+    let tree = parser.parse(code_bytes, None).unwrap();
+    let query = Query::new(&tree_sitter_c::LANGUAGE.into(), "(call_expression(identifier)@a)").unwrap();
+    let mut cursor = QueryCursor::new();
+
+    let mut diff: i32 = 0;
+
+    for qm in cursor.matches(&query, tree.root_node(), code_bytes) {
+        let identifier_node = qm.captures[0].node;
+        let identifier = identifier_node.utf8_text(&code_bytes).unwrap();
+
+        let start = identifier.find("_AC_");
+        let start = match start {
+            None => continue,
+            Some(start) => start
+        };
+
+        let mut identifier = String::from(identifier);
+        identifier.replace_range(start.., "");
+
+        code.replace_range(
+            ((identifier_node.start_byte() as i32) + diff) as usize..
+            ((identifier_node.end_byte() as i32) + diff) as usize, 
+            &identifier);
+        diff += identifier.len() as i32 - identifier_node.byte_range().len() as i32;
+    }
+}
+
+fn treesitter_process() -> Result<(), std::io::Error> {
+    let mut code = fs::read_to_string("./out/out.c")?;
+    let code_bytes = code.clone();
+    let code_bytes = code_bytes.as_bytes();
+    parse_declarator_attributes(&mut code, code_bytes);
+
+    let code_bytes = code.clone();
+    let code_bytes = code_bytes.as_bytes();
+    parse_call_expression_attributes(&mut code, code_bytes);
 
     let mut file = File::create("./out/out.c")?;
     file.write_all(&code.as_bytes())?;
