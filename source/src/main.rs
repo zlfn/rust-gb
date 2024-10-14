7967925678b6179504bcd89cf7d899d259e8b77d
zlfn <ung@zlfn.space>
2024-10-14 14:47:15 +0900

    Refactor: Rename print to gprint

--- source/src/main.rs
+++ source/src/main.rs
@@ -5,25 +5,25 @@
 
 mod gb;
 
-use gb::drawing::{print::{cursor, print}, DmgColor, DrawingStyle};
+use gb::drawing::{print::{cursor, gprint}, DmgColor, DrawingStyle};
 
 #[no_mangle]
 pub extern fn main() {
-    print("TE\nST\0");
+    gprint("TE\nST\0");
 
     cursor(0, 17);
-    print(u8::MAX);
+    gprint(u8::MAX);
 
     DrawingStyle::default()
         .foreground(DmgColor::DarkGrey)
         .apply();
-    print(i8::MIN);
+    gprint(i8::MIN);
 
     DrawingStyle::reversed().apply();
-    print(u16::MAX);
+    gprint(u16::MAX);
 
     DrawingStyle::default().apply();
-    print(i16::MIN);
+    gprint(i16::MIN);
 }
 
 #[allow(unconditional_recursion)]
