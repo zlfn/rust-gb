90269867da524f0fca3e535f4d2443e4df9d2cfd
zlfn <ung@zlfn.space>
2024-10-22 02:22:59 +0900

    feat: GBDK interrupt functions binding

--- source/src/main.rs
+++ source/src/main.rs
@@ -4,23 +4,19 @@
 #![feature(strict_provenance)]
 #![feature(format_args_nl)]
 
-use core::fmt::Write;
-
-use gb::{drawing::{DmgColor, DrawingStream, DrawingStyle}, io::GbStream};
-
 mod gb;
 
 #[no_mangle]
 pub extern fn main() {
-    GbStream::clear();
-    for n in 0..8 as u8 {
-        println!("PLUS{}", n);
+    for i in 0..10000 as u16 {
+        println!("{}", i);
     }
 }
 
 #[panic_handler]
 #[allow(unused_variables)]
 pub fn panic_handler(info: &core::panic::PanicInfo) -> ! {
+    /*
     let mut w = unsafe {DrawingStream::new()};
     DrawingStyle::reversed().apply(&w);
     write!(w, "PANIC occured");
@@ -28,6 +24,7 @@ pub fn panic_handler(info: &core::panic::PanicInfo) -> ! {
     w.cursor(0, 1);
     DrawingStyle::reversed().foreground(DmgColor::LightGrey).apply(&w);
     write!(w, "{}", info.message());
+    */
 
     loop {}
 }
