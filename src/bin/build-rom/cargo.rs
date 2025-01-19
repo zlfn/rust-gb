39a5238e106873290fc72f99acb16d23bdb230e1
zlfn <ung@zlfn.space>
2025-01-19 22:04:04 +0900

    feat: compiler pretty logging

--- src/bin/build-rom/cargo.rs
+++ src/bin/build-rom/cargo.rs
@@ -1,28 +1,39 @@
-use std::process::{self, Command};
+use std::process::{Command, Stdio};
 
 use colored::Colorize;
+use indicatif::ProgressBar;
 
 use crate::{BuildStep, BuildStepError};
 
 pub struct Cargo {}
 
 impl BuildStep for Cargo {
-    fn run(dir: &crate::WorkingDirectory) -> Result<(), BuildStepError> {
-        if let Ok(status) = Command::new("cargo")
-            .args(["build", "--release", "--target-dir", dir.out.as_str()])
-            .status()
-        {
-            if status.success() {
+    fn run(dir: &crate::WorkingDirectory, bar: &ProgressBar) -> Result<(), BuildStepError> {
+        bar.set_message("Rust -> LLVM-IR Compiling...");
+        if let Ok(output) = bar.suspend(|| {
+            Command::new("cargo")
+                .args(["build", "--release", "--target-dir", dir.out.as_str()])
+                .stderr(Stdio::inherit())
+                .output()
+        }) {
+            if output.status.success() {
+                bar.finish_with_message(format!(
+                    "{}",
+                    "Rust -> LLVM-IR Compiling Succeeded".green()
+                ));
                 return Ok(());
             } else {
+                bar.println(String::from_utf8_lossy(&output.stderr));
+                bar.finish_with_message(format!("{}", "Rust -> LLVM-IR Compiling Failed".red()));
                 return Err(BuildStepError::ChildProcessFailed(
                     "cargo".to_string(),
-                    status,
+                    output.status,
                 ));
             }
         } else {
-            println!("{}", "[cargo] cargo execution failed".red());
-            return Err(BuildStepError::ChildExecutionFailed("Cargo".to_string()));
+            bar.println("cargo execution failed");
+            bar.finish_with_message(format!("{}", "Rust -> LLVM-IR Compiling Failed".red()));
+            return Err(BuildStepError::ChildExecutionFailed("cargo".to_string()));
         }
     }
 }
